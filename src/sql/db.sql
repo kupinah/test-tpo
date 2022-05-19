462b943eb3f7950dd99235b631a515ca9aa5eda7
lBrodnikl <lbrodnikluk@gmail.com>
2022-05-19 12:49:42 +0200

    sql to create a database

---
+++ src/sql/db.sql
@@ -0,0 +1,50 @@
+CREATE TABLE Uporabnik(
+	uporabnikId int,
+	ime varchar(20),
+	priimek varchar(20),
+	uporabniskoIme varchar(20),
+	obcinaId int,
+	email varchar(50),
+	geslo varchar(50),
+	slika bytea,
+	soprocila int[],
+	primary key (uporabnikId)
+);
+
+CREATE TABLE Sporocilo(
+	sporociloId int,
+	posiljatelj int,
+	prejemnik int,
+	vsebina varchar[],
+	primary key (sporociloId)
+);
+
+CREATE TABLE Novica(
+	novicaId int,
+	naslov varchar(100),
+	avtorId int,
+	datum date,
+	vsebina varchar(10000),
+	obcinaId int,
+	lokacija varchar(500),
+	primary key (novicaId)
+);
+
+CREATE TABLE Predlog(
+	predlogId int,
+	naslov varchar(100),
+	avtorId int,
+	datum date,
+	vsebina varchar(10000),
+	obcinaId int,
+	steviloVseckov int,
+	steviloNevseckov int,
+	lokacija varchar(500),
+	primary key (predlogId)
+);
+
+alter table Novica add foreign key (avtorId) references Uporabnik(uporabnikId);
+
+alter table Predlog add foreign key (avtorId) references Uporabnik(uporabnikId);
+
+alter table Sporocilo add foreign key (posiljateljId) references Uporabnik(uporabnikId), add foreign key (prejemnikId) references Uporabnik(uporabnikId);
