07208188a0ab2732db79d0a40b0642d456a66788
LukaMarkicevic <luka.markicevic@gmail.com>
2022-05-19 12:35:00 +0200

    homepage work #1

---
+++ src/obcinski/src/routes/Router.js
@@ -0,0 +1,30 @@
+import { lazy } from "react";
+import { Navigate } from "react-router-dom";
+
+/****Layouts*****/
+const FullLayout = lazy(() => import("../layouts/FullLayout.js"));
+
+/***** Pages ****/
+
+const Starter = lazy(() => import("../views/Starter.js"));
+const About = lazy(() => import("../views/About.js"));
+const Cards = lazy(() => import("../views/ui/Cards"));
+const Forms = lazy(() => import("../views/ui/Forms"));
+
+/*****Routes******/
+
+const ThemeRoutes = [
+  {
+    path: "/",
+    element: <FullLayout />,
+    children: [
+      { path: "/", element: <Navigate to="/starter" /> },
+      { path: "/starter", exact: true, element: <Starter /> },
+      { path: "/about", exact: true, element: <About /> },
+      { path: "/cards", exact: true, element: <Cards /> },
+      { path: "/forms", exact: true, element: <Forms /> },
+    ],
+  },
+];
+
+export default ThemeRoutes;
---
+++ src/obcinski/src/views/About.js
@@ -0,0 +1,9 @@
+import { Row, Col, Card, CardBody, CardTitle, Button } from "reactstrap";
+
+const About = () => {
+  return (
+    <div>CAO LUKA</div>
+  );
+};
+
+export default About;
